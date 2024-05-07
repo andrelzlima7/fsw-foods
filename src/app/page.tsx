f86807a73639bbf0ed5e8ab81974472c078450e9
André Luiz de Lima <andrelzlima7@gmail.com>
2024-05-07 15:54:02 -0300

    feat: adicionado componente Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import Header from "@/components/header";
+
 export default function Home() {
-  return <main></main>;
+  return <main>
+    <Header />
+  </main>;
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between px-5 pt-7">
+      <Image src="/logo-fsw.png" alt="logo fsw" width={100} height={30} />
+      <Button variant={"outline"} className="border-none">
+        <Menu />
+      </Button>
+    </header>
+  );
+};
+
+export default Header;
