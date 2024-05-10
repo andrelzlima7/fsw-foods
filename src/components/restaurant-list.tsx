1b9adcd1cbce6b9e681f7ca0355fb3f91d6288c3
André Luiz de Lima <andrelzlima7@gmail.com>
2024-05-10 10:23:50 -0300

    feat: adicionado ordenação para mostrar os melhores restaurantes

--- src/components/restaurant-list.tsx
+++ src/components/restaurant-list.tsx
@@ -1,10 +1,10 @@
 import { ROUTE_RESTAURANTS } from "@/api/routes";
 import RecommendedItems from "./recommended-items";
 import Image from "next/image";
-import { Bike, TimerReset } from "lucide-react";
+import { Bike, Heart, Star, TimerReset } from "lucide-react";
 
 interface Restaurants {
-  id: string
+  id: string;
   name: string;
   imageUrl: string;
   deliveryFee: number;
@@ -16,12 +16,15 @@ interface Restaurants {
 const RestaurantList = async () => {
   const response = await fetch(ROUTE_RESTAURANTS);
   const restaurants: Restaurants[] = await response.json();
+
+  const orderRestaurants = restaurants.sort((a,b) => b.stars - a.stars)
+  
   return (
     <div>
       <RecommendedItems title="Restaurantes Recomendados" url="" />
       <div>
         <ul className="ml-5 mt-4 flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {restaurants.slice(0, 5).map((restaurant, id) => {
+          {orderRestaurants.slice(0, 5).map((restaurant) => {
             return (
               <li key={restaurant.id}>
                 <div className="relative h-[136px] w-[266px]">
@@ -34,11 +37,17 @@ const RestaurantList = async () => {
                     sizes="100%"
                     quality={100}
                   />
-                  <div className="absolute left-2 top-2  rounded-xl bg-primary text-xs font-semibold text-secondary">
-                    <span className="flex items-center p-1">{}</span>
+                  <div className="absolute left-2 top-2  rounded-xl bg-secondary text-xs font-semibold">
+                    <span className="flex items-center p-1 gap-1 px-2">
+                      <Star fill="#FFB100" stroke="" size={10} />
+                      {(restaurant.stars).toFixed(1)}
+                    </span>
                   </div>
                   <div className="absolute right-2 top-2  rounded-xl bg-primary text-xs font-semibold text-secondary">
-                    <span className="flex items-center p-1">{restaurant.like}</span>
+
+                    <span className="flex items-center justify-center rounded-full p-1">
+                      <Heart size={16} />
+                    </span>
                   </div>
                 </div>
                 <div className="mt-3">
